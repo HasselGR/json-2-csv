73e370cf277409deca2b645ac6241d3a02792eec
unknown <gamboadavid1998@gmail.com>
2022-09-11 18:32:37 -0400

    style and align changes

--- src/App.js
+++ src/App.js
@@ -102,51 +102,55 @@ function App() {
 
       <ReactCardFlip isFlipped={json2csv}>
         <div className="front">
-          <h1>JSON-2-CSV</h1>
+          <div className='header-container'>
+            <h1>JSON-2-CSV</h1>
+            <img className='swap-image' onClick={toggleMode} src={ChangeMode} alt="change"/>
+          </div>
           <h2>The objective of JSON2CSV is to help bridge the gap between JSON and CSV by converting JSON to CSV to make it easier to review data in a spreadsheet.
             It allows the user to paste JSON into a text box to generate its equivalent CSV.</h2>
           <div className="transform-container">
             <div className='box-container'>
-            <h2>Please input your JSON text here</h2>
-            <input className='input-container' type="text" name="jsontext" value={jsonTransform} placeholder='{name:value}' onChange={jsonChange}></input>
-            <button onClick={transformJSON}>Convert</button>
+              <h2>Please input your JSON text here</h2>
+              <input className='input-container' type="text" name="jsontext" value={jsonTransform} placeholder='{name:value}' onChange={jsonChange}></input>
+              <button className='button-74' onClick={transformJSON}>Convert</button>
+            </div>
+            <div className='box-container'>
+              <h2>You will see your text transformed to CSV Here</h2>
+              <input className='input-container' type="text" name="CSV" value={csv} placeholder=''></input>
+              <button className='button-74' onClick={clearContents}>Clear</button> 
+            </div>
           </div>
           <div className='box-container'>
-            <h2>You will see your text transformed to CSV Here</h2>
-            <input className='input-container' type="text" name="CSV" value={csv} placeholder=''></input>
-            <button onClick={clearContents}>Clear</button>
-            <button onClick={saveFile}>Save CSV file into computer</button>
-          </div>
-          <div className='box-container'>
+          <button className='button-74' onClick={saveFile}>Save CSV file into computer</button>
             <h2>You can also upload a file to be converted to CSV</h2>
             <input  type="file" name="JSONfile" accept=".json" onChange={fileLoadJSON}></input>
           </div>
-          <button className='changeStyle-button' onClick={toggleMode}><img src={ChangeMode} alt="change"/></button>
-          
-          </div> 
         </div>
         
         <div className='back'>
-          <h1>CSV-2-JSON</h1>
-          <h2>In CSV2JSON you'll start by copying the JSON2CSV app you created and then modify it to allow CSV to JSON conversion as well the JSON to CSV conversion that's already present.
-            In additional to providing a useful function, this challenge will also give you practice in modifying existing applications to add new functionality.
-          </h2>
+          <div className='header-container'>
+            <h1>CSV-2-JSON</h1>
+            <img className='swap-image' onClick={toggleMode} src={ChangeMode} alt="change"/>
+          </div>
+            <h2>
+              In CSV2JSON you'll start by copying the JSON2CSV app you created and then modify it to allow CSV to JSON conversion as well the JSON to CSV conversion that's already present.
+              In additional to providing a useful function, this challenge will also give you practice in modifying existing applications to add new functionality.
+            </h2>
           <div className="transform-container "> 
             <div className='box-container'>
               <h2>Please input your CSV text here</h2>
-              <input className='input-container' type="text" name="csvtext" value={csvTransform} placeholder='name1, name2, name3, value1, value2, value3' onChange={csvChange}></input>
-              <button onClick={transformCSV}>Convert</button>
+              <input className='back-input-container' type="text" name="csvtext" value={csvTransform} placeholder='name1, name2, name3, value1, value2, value3' onChange={csvChange}></input>
+              <button className='button-74' onClick={transformCSV}>Convert</button>
             </div>
             <div className='box-container'>
               <h2>You will see your text transformed to JSON Here</h2>
-              <input className='input-container' type="text" name="jsontransformed" value={json} placeholder=''></input>
-              <button onClick={clearContents}>Clear</button>
+              <input className='back-input-container' type="text" name="jsontransformed" value={json} placeholder=''></input>
+              <button className='button-74' onClick={clearContents}>Clear</button>
             </div>
-            <div className='box-container'>
-            <h2>You can also upload a file to be converted to JSON</h2>
-            <input  type="file" name="CSV" accept=".csv" onChange={fileLoadCSV}></input>
           </div>
-          <button className='changeStyle-button' onClick={toggleMode}><img src={ChangeMode} alt="change"/></button>
+          <div className='box-container'>
+            <h2>You can also upload a file to be converted to JSON</h2>
+            <input type="file" name="CSV" accept=".csv" onChange={fileLoadCSV}></input>
           </div>
         </div>
       </ReactCardFlip>
